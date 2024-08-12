e9ca1a57e57f072c0707ab9d82401514e380c6cc
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 13:47:55 +0530

    Create Constants.java

---
+++ src/main/java/org/example/util/Constants.java
@@ -0,0 +1,12 @@
+package org.example.util;
+
+public class Constants{
+
+  public static final String CSV_FILE_ADDRESS="src/main/resources/employee.csv";
+
+  public static final int MIN_MANAGER_SALARY_PERCENTAGE=20;
+
+  public static final int MAX_MANAGER_SALARY_PERCENTAGE=50;
+
+  public static final int MAX_HIERARCHY_LEVEL=4;
+}
