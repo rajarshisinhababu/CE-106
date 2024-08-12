9d3965e3fa3105f8801609f1d09e94e002cdaedd
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 14:01:25 +0530

    Create CsvReader.java

---
+++ src/main/java/org/example/util/CsvReader.java
@@ -0,0 +1,40 @@
+package org.example.util;
+
+public class CsvReader{
+
+  public List<Employee> readCsv(String path) throws IOException {
+
+    //get the csv parser from the path
+    CSVParser parser = getCsvParser(path);
+
+    //parse the csvfile and return the list of employess
+    return parseCsv(parser);
+  }
+
+  private List<Employee> parseCSV(CSVParser parser){
+
+    List<Employee> employees = new ArrayList<Employee>();
+
+    for(CSVRecord record : parser){
+      Employee employee = new Employee();
+      employee.setId(Integer.ParseInt(record.get(EmployeeHeaders.id.name())));
+      employee.setFirstName();
+
+      employees.add(employee);
+    }
+  }
+
+  private CSVParser getCSVParser(String path) throws IOException {
+    Reader reader = Files.newBufferedReader(Paths.get(path));
+
+    return new CSVParser(reader , CSVFormat.DEFAULT
+                         .withFirstRecordAsHeader()
+                         .withIgnoreheaderCase()
+                         .withTrim());
+  }
+
+  private int getManagerId(CSVRecord record){
+      String managerId
+  }
+
+}
