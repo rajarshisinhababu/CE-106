398a26f565e05d50f5ac061df6e15ff491ebdb39
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 16:10:36 +0530

    Update EmployeeUtil.java

--- src/main/java/org/example/util/EmployeeUtil.java
+++ src/main/java/org/example/util/EmployeeUtil.java
@@ -8,10 +8,24 @@ import org.example.entity.Employee;
 */
 public class EmployeeUtil{
 
+  /**
+ * Calculate the percentage difference between manager and subordinates salary.
+ *
+ * @param managerSalary      the salary of the manager
+ * @param subordinatesSalary the total salary of the subordinates
+ * @return the percentage difference between manager and subordinates salary
+ */
   public int calPercentage(int managerSalary, int subordinateSalary){
     return Math.abs(managerSalary-subordinateSalary)*100 / managerSalary;
   }
 
+  /**
+ * Get employee name from employee id.
+ *
+ * @param empId        the employee id
+ * @param empHierarchy the employee hierarchy containing employee details
+ * @return the full name of the employee
+ */
   public String getEmpNameFromId(int empId, EmpHierarchy empHierarchy){
     Employee emp =   empHierarchy.getEmployeeMap().get(empId);
 
