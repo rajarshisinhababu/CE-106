bb444ebcc84f467f0e202933e2ddd90bb988d298
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 17:36:34 +0530

    Update EmployeeService.java

--- src/main/java/org/example/component/EmployeeService.java
+++ src/main/java/org/example/component/EmployeeService.java
@@ -18,19 +18,19 @@ public class EmployeeService{
       //get all the subordinates of a manager
       List<Integer> subordinates = empHierarchy.getEmpHierarchy().get(managerId);
 
-      if(subOrdinates.isEmpty())continue;
+      if(subordinates.isEmpty())continue;
 
       int totalSubSalary = subordinates.stream().
                         mapToInt(empId -> empHierarchy.getEmployeeMap().get(empId).getSalary()).
                         sum();
       int managerSalary = empHierarchy.getEmployeeMap().get(managerId).getSalary();
-      int avgSubSalary = totalSubSalary/subOrdinates.size();
+      int avgSubSalary = totalSubSalary/subordinates.size();
       int underPaidSal = avgSubSalary + (int) (avgSubSalary * 0.2);
       int overPaidSal = avgSubSalary + (int) (avgSubSalary * 0.5);
       int salaryPercent = employeeUtil.calPercentage(managerSalary,avgSubSalary);
 
       if(managerSalary < underPaidSal && salaryPercent < Constants.MIN_MANAGER_SALARY_PERCENTAGE){
-        System.out.println("Manager Id :"+employeeUtil.getEmpNamefromId(managerId,empHierarchy)+" is underpaid by "+ (underPaidSal - managerSalary));      
+        System.out.println("Manager Id :"+employeeUtil.getEmpNameFromId(managerId,empHierarchy)+" is underpaid by "+ (underPaidSal - managerSalary));      
       }else if(managerSalary> overPaidSal && salaryPercent > Constants.MAX_MANAGER_SALARY_PERCENTAGE){
         System.out.println("Manager Id:"+employeeUtil.getEmpnameFromId(managerId,empHierarchy)+ " is overpaid by "+ (managerSalary- overPaidSal));
       }    
