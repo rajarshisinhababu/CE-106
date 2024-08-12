2b9a8a056c894d163cd64b13686d840b37061bff
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 14:35:37 +0530

    Create EmployeeUnitTest.java

---
+++ src/test/java/org/example/util/EmployeeUnitTest.java
@@ -0,0 +1,17 @@
+package org.example.util;
+
+
+class EmployeeUnitTest{
+
+  private EmployeeUtil employeeUtil;
+  EmpHiearchy empHieararchy;
+
+  @BeforeEach
+  void setup(){
+
+  }
+
+  @Test
+
+
+}
