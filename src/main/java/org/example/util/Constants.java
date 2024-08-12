74a3e8121cebaf4310dd6e5be2e83efd82ff6af8
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 16:12:27 +0530

    Update Constants.java

--- src/main/java/org/example/util/Constants.java
+++ src/main/java/org/example/util/Constants.java
@@ -2,11 +2,15 @@ package org.example.util;
 
 public class Constants{
 
+  // Path to the employee CSV file
   public static final String CSV_FILE_ADDRESS="src/main/resources/employee.csv";
 
+  // Minimum percentage difference between manager and subordinates' average salary
   public static final int MIN_MANAGER_SALARY_PERCENTAGE=20;
 
+  // Maximum percentage difference between manager and subordinates' average salary
   public static final int MAX_MANAGER_SALARY_PERCENTAGE=50;
 
+  // Maximum hierarchy level allowed between an employee and the CEO
   public static final int MAX_HIERARCHY_LEVEL=4;
 }
