9a067190354a5cb95f136f6f17994c8eafaf6fbc
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 16:23:20 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,20 +12,19 @@ import java.util.list;
 public class Main{
 
   public static void main(String[] args){
-
-  //load employee data from csv
-  List<Employee> employees = new EmployeeService().loadEmployeeDataFromCSV(Constants.CSV_FILE_ADDRESS);
-
-  //create the employee hierarchy
-  EmpHierarchy empHierarchy = new EmpHierarchy();
-  empHierarchy.createEmployeeHierarchy(employees);
-
-  //get underpaid/overpaid managers
-  EmployeeService employeeService = new EmployeeService();
-  employeeService.getPayDifference(empHierarchy);
-
-  //get employees who have more than 4 managers
-  System.out.println();
-  employeeService.getEmployeeWithMostHierarchy(empHierarchy);
-  }
+	  //load employee data from csv
+	  List<Employee> employees = new EmployeeService().loadEmployeeDataFromCSV(Constants.CSV_FILE_ADDRESS);
+	
+	  //create the employee hierarchy
+	  EmpHierarchy empHierarchy = new EmpHierarchy();
+	  empHierarchy.createEmployeeHierarchy(employees);
+	
+	  //get underpaid/overpaid managers
+	  EmployeeService employeeService = new EmployeeService();
+	  employeeService.getPayDifference(empHierarchy);
+	
+	  //get employees who have more than 4 managers
+	  System.out.println();
+	  employeeService.getEmployeeWithMostHierarchy(empHierarchy);
+	}
 }
