1a3d3090a785248aeb85f3b1f605404bc5639fef
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 13:45:08 +0530

    Create Employee.java

---
+++ src/main/java/org/example/entity/Employee.java
@@ -0,0 +1,10 @@
+package org.example.entity
+
+public class Employee{
+
+  private int id;
+  private String firstName;
+  private String lastName;
+  private int salary;
+  private int managerId;
+}
