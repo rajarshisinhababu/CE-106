0b415406f7a73ace65514245c6472f079d26728a
rajarshisinhababu <38199574+rajarshisinhababu@users.noreply.github.com>
2024-08-12 17:53:06 +0530

    Update EmployeeUnitTest.java

--- src/test/java/org/example/util/EmployeeUnitTest.java
+++ src/test/java/org/example/util/EmployeeUnitTest.java
@@ -4,7 +4,7 @@ import org.example.component.EmpHierarchy;
 import org.example.entity.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.unit.List;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmployeeUnitTest{
@@ -14,51 +14,51 @@ class EmployeeUnitTest{
 
   @BeforeEach
   void setup(){
-    empHierarchy = new EmpHierarchy();
+    empHieararchy = new EmpHierarchy();
     employeeUtil = new EmployeeUtil();
     Employee emp1 = new Employee();
     emp1.setId(1);
     emp1.setFirstName("John");
     emp1.setLastName("Doe");
     emp1.setSalary(1000);
-    emp1.setManager(0);
+    emp1.setManagerId(0);
     Employee emp2 = new Employee();
     emp2.setId(2);
     emp2.setFirstName("Jane");
     emp2.setLastName("Smith");
     emp2.setSalary(800);
     emp2.setManagerId(1);
-    List<Employee> employees = List.of(empp1,emp2);
-    empHieararchy.createEmployeeHieararchy(employee);
+    List<Employee> employees = List.of(emp1,emp2);
+    empHieararchy.createEmployeeHieararchy(employees);
   }
 
   @Test
   void calPercentage_shoudReturnCorrectPerOfDiff(){
       int result = employeeUtil.calPercentage(1000,1000);
-      assertEqual(0,result);
+      assertEquals(0,result);
   }
 
   @Test
   void calPercentage_shoulReturnZerodSalaryEqual(){
       int result = employeeUtil.calPercentage(1000,1000);
-      assertEqual(0,result);
+      assertEquals(0,result);
   }
 
   @Test
   void calPercentage_shouldReturnCorrectPercentageWhenManagerSalaryLess(){
-    int result = employeeUti.calPercent(800,1000);
-    assertEqual(25,result);
+    int result = employeeUtil.calPercent(800,1000);
+    assertEquals(25,result);
   }
 
   @Test
   void getEmpFromId_shouldReturnCorrectEmpName(){
-    String result = empUtil.getEmpNameFromId(1,empHierarchy);
+    String result = employeeUtil.getEmpNameFromId(1,empHieararchy);
     assertEquals("John Doe", result);
   }
 
   @Test
   void getEmpFromId_shouldReturnNullWhenNotFound(){
-    String result = empUtil.getEmpNameFromId(19,empHieararchy);
+    String result = employeeUtil.getEmpNameFromId(19,empHieararchy);
     assertEquals(null,result);
   }
 
